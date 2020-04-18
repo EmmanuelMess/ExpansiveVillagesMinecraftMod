216b7970a72b0b59c29bd471e4f1ba985f3e8d64
Emmanuel M <emmanuelbendavid@gmail.com>
2020-04-18 17:12:03 -0300

    Simple spawn logic

--- src/main/java/ar/com/messupetru/expansivevillages/CreateBabyVillagerEvent.java
+++ src/main/java/ar/com/messupetru/expansivevillages/CreateBabyVillagerEvent.java
@@ -2,6 +2,7 @@ package ar.com.messupetru.expansivevillages;
 
 import net.minecraft.entity.merchant.villager.VillagerEntity;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,8 +11,17 @@ public class CreateBabyVillagerEvent extends LivingSpawnEvent {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public CreateBabyVillagerEvent(VillagerEntity entity, IWorld world, double x, double y, double z) {
-        super(entity, world, x, y, z);
+    public final ServerWorld serverWorld;
+    public final VillagerEntity child;
+
+    public CreateBabyVillagerEvent(VillagerEntity child, ServerWorld world, double x, double y, double z) {
+        super(child, world, x, y, z);
+        if(!child.isChild()) {
+            throw new IllegalArgumentException("Child is not child!");
+        }
+
+        serverWorld = world;
+        this.child = child;
     }
 
 }
--- src/main/java/ar/com/messupetru/expansivevillages/ExpansiveVillagesMod.java
+++ src/main/java/ar/com/messupetru/expansivevillages/ExpansiveVillagesMod.java
@@ -1,20 +1,29 @@
 package ar.com.messupetru.expansivevillages;
 
 import net.minecraft.entity.merchant.villager.VillagerEntity;
+import net.minecraft.pathfinding.Path;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.village.PointOfInterest;
+import net.minecraft.village.PointOfInterestManager;
+import net.minecraft.village.PointOfInterestType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExpansiveVillagesMod.MOD_ID)
 public class ExpansiveVillagesMod {
     public static final String MOD_ID = "expansivevillages";
+
+    public static final HashMap<UUID, HouseCreatorManager> HOUSE_CREATOR_MANAGER = new HashMap<>();
 
     private static final Logger LOGGER = LogManager.getLogger();
 
@@ -22,16 +31,37 @@ public class ExpansiveVillagesMod {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+    public static class HouseCreatorManager {
+        private static final int MAX_SPAWN_PROBABILITY = 80;
+        private int spawnProbability = 0;
+        
+        public void setAmountOfBeds(long amountFromChild) {
+            spawnProbability = (int) (MAX_SPAWN_PROBABILITY - amountFromChild * 10);
+        }
+    }
+
     @Mod.EventBusSubscriber(modid = ExpansiveVillagesMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
     public static class RegistryEvents {
         @SubscribeEvent
         public static void onNewbornVillager(final CreateBabyVillagerEvent event) {
-            VillagerEntity villager = (VillagerEntity) event.getEntityLiving();
-            if(villager.isChild()) {
-                LOGGER.info("Newborn!");
+            VillagerEntity child = event.child;
 
-            }
-            event.setResult(Event.Result.DEFAULT);
+            java.util.function.Predicate<PointOfInterestType> type = PointOfInterestType.HOME.func_221045_c();
+            BlockPos villagerPosition = new BlockPos(child);
+            int distance = 48;
+            Stream<PointOfInterest> pointOfInterestStream = event.serverWorld.getPointOfInterestManager()
+                    .func_219146_b(type, villagerPosition, distance, PointOfInterestManager.Status.HAS_SPACE)
+                    .filter((pointOfInterest) -> pathfindToBed(child, pointOfInterest.getPos()));
+
+            HOUSE_CREATOR_MANAGER.putIfAbsent(child.getUniqueID(), new HouseCreatorManager());
+            HOUSE_CREATOR_MANAGER.get(child.getUniqueID()).setAmountOfBeds(pointOfInterestStream.count());
+
+            LOGGER.info("Newborn " + child.getPosition().toString() + ": ");
+        }
+
+        private static boolean pathfindToBed(VillagerEntity villager, BlockPos blockPos) {
+            Path path = villager.getNavigator().getPathToPos(blockPos, PointOfInterestType.HOME.func_225478_d());
+            return path != null && path.func_224771_h();
         }
     }
 }
